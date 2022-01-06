1551b95dfd277d1df7cdbca27d4d5afd0556962f
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-01-06 22:37:57 +0300

    added error on submit button form

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,36 @@
+// couner number running
 $('.counter').countUp();
+
+// form custom error message
+
+// window.addEventListener("DOMContentLoaded", function(){
+//   var femail = document.getElementById("femail");
+//   femail.addEventListener("input", function(){
+//     if (femail.validity.typeMismatch || femail === '') {
+//       femail.setCustomValidity("Check your email");
+//       femail.reportValidity();
+//     } else {
+//       femail.setCustomValidity("");
+//     }
+//   });
+// });
+
+function check () {
+  // (C1) INIT
+  var valid = true, error = "", field = "";
+
+  // (C2) NAME
+  field = document.getElementById("femail");
+  error = document.getElementById("input-error");
+  if (!field.checkValidity()) {
+    valid = false;
+    field.classList.add("error");
+    error.classList.add("error-msg");
+  } else {
+    field.classList.remove("error");
+    error.classList.remove("error-msg");
+  }
+
+  // (C4) RESULT - make it 'valid' for working
+  return false;
+}
